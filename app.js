b26a19ddc457a72452802ea614ace0f70ca1dfdf
Leonardo T. S <66446331+LeonardoTS-ETEC-Barueri@users.noreply.github.com>
2021-02-21 11:20:24 -0300

    Pequenos ajustes e correcoes.
    • Removi algumas linhas desnecessárias no entry-point do app.
    • Fiz o diretório de arquivos estáticos funcionar.
    • Ajustei alguns pontos da responsividade na view "login".
    • Configurei o Nodemon como dev-dependency e deixei um script pra que ele execute o servidor HTTP via npm.
    
    Boa sorte ai Gustavo! Manda ver que o negócio ta ficando bão :D

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
-const express = require('express');
+// Importações.
+	const express = require('express');
+	const app = express();
 
-const app = express();
+	const path = require('path');
 
-const path = require('path')
+// Configurações.
 
-app.set('views','./views')
+	// View Engine.
+		// app.set('views','./views');	// Não é necessário, a EJS entende o diretório "views" na raiz da aplicação como padrão para Views.
+		app.set("view engine", "ejs");
 
-app.set("view engine", "ejs");
+	// Arquivos Estáticos(Públicos) --- Cuidado, todo contéudo dentro desse diretório pode ser fácilmente acessado pelos usuários finais de forma irrestrita.
+		app.use('/public', express.static(path.join(__dirname, "public")));	
+			// Tente dar um [ Console.log(path.join(__dirname, "public")); ] em uma Middleware qlqr e atualize a página no navegador com a aplicação rodando.
+			// Veja o resultado no console. (Deverá ser o caminho absoluto para a pasta "public")
 
-app.use(express.static(__dirname+ '/public'));
 
-app.get('/',(req, res) => {
-	var usuario = {
-		"nome": "Gustavo",
-		"idade": 16,
-		"sexo": "Masculino"
-	}
-	res.render("home",{'user': usuario})
+// Gerenciamento das Rotas.
+	app.get('/',(req, res) => {
+		var usuario = {
+			"nome": "Gustavo",
+			"idade": 16,
+			"sexo": "Masculino"
+		}
+		res.render("home",{'user': usuario})
+	});
 
-})
-app.get('/login',function(req,res){
+	app.get('/login',function(req,res){
+		res.render("login")
+	});
 
-	res.render("login")
-
-})
-
+// Execução dos Request Listeners do servidor HTTP. Faz o servidor rodar :D
 app.listen(8081,function(){
+	console.log('Servidor HTTP inicializado no endereço: "https://localhost:8081"');
+});
 
-	console.log ('executando na porta 8081')
 
-})
+/**
+ Observações: 	Incluí o módulo do nodemon como dependência de desenvolvimento (npm install --save-dev nodemon) - Veja o "package.json".
+				Além disso, incluí um script para executar o servidor de forma mais ágil com o próprio nodemon.
+				Inicie o servidor usando este comando no terminal:	"npm run dev" --- "npm run" executa os "scripts" configurados no "package.json".
+
+				Também ajustei algumas pequenas coisas na View "login.ejs". Dá uma olhada lá.
+				Boa sorte manin! Alguns pequenos ajustes são necessários, mas você tá aprendendo muito rápido a aplicar os conceitos dessa tecnologia, parabéns e obrigado! \o
+ */
